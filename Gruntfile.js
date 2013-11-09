7ee5eb200d00fd5607988f22e8b0d7d0ddcd2244
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-09 16:26:28 +0100

    Single run tests, so travis will report that it works

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,8 @@ module.exports = function (grunt) {
     },
     karma: {
       unit: {
-        configFile: 'karma.conf.js'
+        configFile: 'karma.conf.js',
+        singleRun: true
       }
     },
     ngmin: {
